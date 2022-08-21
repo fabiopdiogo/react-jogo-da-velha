60a9b286b4cb7976005b57f7f67f459000ae8108
Fabio Diogo <fabindiogo@gmail.com>
2022-08-21 20:06:41 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,18 @@ import styles from './Header.module.css'
 
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Fabio Diogo</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/fabiopdiogo/react-jogo-da-velha"/>
+      </div>
     </div>
+   
   )
 }
 
