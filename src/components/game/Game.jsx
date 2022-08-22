aedaaed40d93ee97ff20c8b7cb7dffe6f3308995
Fabio Diogo <fabindiogo@gmail.com>
2022-08-22 08:40:37 -0300

    Criando estados no gameOption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,23 @@
+import { useState } from 'react'
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
+let array = Array(9).fill(0)
 
 function Game (){
+  const [gameState, setGameState] = useState(Array(9).fill())
+
+  console.log(gameState)
   return(
     <div className={styles.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
+      {
+        gameState.map((value, pos) => 
+          <GameOption
+            key={`game-option-pos-${pos}`}
+            status={value}
+          />
+        )
+      }      
     </div>
       
   )
