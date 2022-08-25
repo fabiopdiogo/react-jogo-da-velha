62e3c171bd9337039568c413dc17fbca7c46c5f4
Fabio Diogo <fabindiogo@gmail.com>
2022-08-25 09:46:01 -0300

    verificado empate

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -17,9 +17,11 @@ const winnerTable =[
 
 function Game (){
   const [gameState, setGameState] = useState(Array(9).fill(0))
-  const [currentPlayer, setCurrentPlayer] = useState(1)
+  const [currentPlayer, setCurrentPlayer] = useState(-1)
   const [winner,setWinner] = useState(0)
   const [winnerLine, setWinnerLine]= useState([])
+  const [draw,setDraw] = useState(false)
+
   const handleClick = (pos) =>{
       if (gameState[pos] === 0  && winner === 0) {
         let newGameState = [...gameState]
@@ -44,6 +46,14 @@ function Game (){
     setGameState(Array(9).fill(0))
     setWinner(0)
     setWinnerLine([])
+    setDraw(false)
+  }
+
+  const verifyDraw = () => {
+    if (gameState.find((value) => value === 0) === undefined && winner === 0){
+      setDraw(true)
+    }
+
   }
 
   const verifyWinnerLine = (pos) =>
@@ -52,8 +62,12 @@ function Game (){
   useEffect(() => { 
     setCurrentPlayer(currentPlayer * -1)
     verifyGame()
+    verifyDraw()
   }, [gameState]); 
   
+  useEffect(() => {
+    if (winner !== 0) setDraw(false)
+  },[winner])
 
   return(
     
@@ -66,6 +80,7 @@ function Game (){
               status={value}
               onClick={() => handleClick(pos)}
               isWinner={verifyWinnerLine(pos)}
+              isDraw={draw}
             />
           )
         }      
@@ -74,6 +89,7 @@ function Game (){
         currentPlayer={currentPlayer}
         winner={winner}
         onReset={handleReset}
+        isDraw={draw}
       />
     </div>
       
