e602c4691dbf014972bf8189fe63cdb61146a620
Fabio Diogo <fabindiogo@gmail.com>
2022-09-07 16:01:25 -0300

    Adicionando o titulo na pagina

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,14 @@
-import '../style.css'
+import '../src/style.css'
+import Head from 'next/head'
 
 function App ({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>Jogo da Velha - Fabio Diogo</title>
+    </Head>
     <Component {...pageProps} />
+  </>
   )
 }
 
